41956eb203a4015ea57c88e7587e3d0a1517db82
gabzzel <45881481+gabzzel@users.noreply.github.com>
2024-02-19 14:00:06 +0100

    Added more robustness to e57 reading

--- PointCloud.py
+++ PointCloud.py
@@ -4,34 +4,39 @@ import pye57  # https://pypi.org/project/pye57/
 import laspy  # https://pypi.org/project/laspy/
 import plyfile  # https://pypi.org/project/plyfile/ and https://python-plyfile.readthedocs.io/en/latest/index.html
 from plyfile import PlyData
+from pye57 import e57
 
 
 class PointCloud:
     def __init__(self):
+        self.points_default_dtype = np.dtype(np.float32)
         self.points: np.ndarray = None
+
+        self.intensities_default_dtype = np.dtype(np.float32)
         self.intensities: np.ndarray = None  # Default type is np.float32
+
+        self.color_default_dtype = np.dtype(np.uint8)
         self.colors: np.ndarray = None  # Default type is np.uint8
 
     # https://pypi.org/project/pye57/
     def read_e57(self, filename: str):
-        e57 = pye57.E57(filename)
-        raw_data = e57.read_scan_raw(0)
+        e57_object = pye57.E57(filename)
+        data = e57_object.read_scan_raw(0)
+        header: pye57.ScanHeader = e57_object.get_header(0)
 
-        x = raw_data["cartesianX"]
-        y = raw_data["cartesianY"]
-        z = raw_data["cartesianZ"]
-        self.points = np.stack((x, y, z), axis=1)
-        # TODO read some kind of scaling value so we can convert it correctly.
+        fm = self.map_field_names(header.point_fields)  # Field mapping
 
-        if "intensity" in raw_data.keys():
-            self.intensities = raw_data["intensity"]
+        self.points = np.stack((data[fm['x']], data[fm['y']], data[fm['z']]), axis=1) \
+            if fm['x'] and fm['y'] and fm['z'] \
+            else np.zeros(shape=(header.point_count, 3), dtype=self.points_default_dtype)
 
-        if "colorRed" in raw_data.keys() and "colorGreen" in raw_data.keys() and "colorBlue" in raw_data.keys():
-            color_normalization_factor: float = 255.0  # TODO, normalize by value read from header or data.
-            r = raw_data["colorRed"].astype(np.float64)
-            g = raw_data["colorGreen"].astype(np.float64)
-            b = raw_data["colorBlue"].astype(np.float64)
-            self.colors = np.stack((r, g, b), axis=1) / color_normalization_factor
+        self.intensities = data[fm['intensity']] \
+            if fm['intensity'] \
+            else np.zeros(shape=(header.point_count,), dtype=self.intensities_default_dtype)
+
+        self.colors = np.stack((data[fm['r']], data[fm['g']], data[fm['b']]), axis=1) \
+            if fm['r'] and fm['g'] and fm['b'] \
+            else np.zeros(shape=(header.point_count, 3), dtype=self.color_default_dtype)
 
     # https://pypi.org/project/pye57/
     def write_e57(self, filename: str):
@@ -42,14 +47,16 @@ class PointCloud:
         raw_data["cartesianY"] = self.points[:, 1]
         raw_data["cartesianZ"] = self.points[:, 2]
 
-        if self.intensities is not None:
-            raw_data["intensity"] = self.intensities
+        raw_data["intensity"] = self.intensities
 
-        if self.colors is not None:
-            # e57 expects the color data to be ints between 0 and 255 (incl.)
-            raw_data["colorRed"] = (self.colors[:, 0] * 255).astype(np.int32)
-            raw_data["colorGreen"] = (self.colors[:, 1] * 255).astype(np.int32)
-            raw_data["colorBlue"] = (self.colors[:, 2] * 255).astype(np.int32)
+        # e57 expects the color data to be ints between 0 and 255 (incl.)
+
+        # If we have float RGB data, we expect it to be in [0,1]
+        if np.issubdtype(self.colors.dtype, np.floating):
+            max_value = np.iinfo(np.uint8).max
+            raw_data["colorRed"] = (self.colors[:, 0] * max_value).astype(np.uint8)
+            raw_data["colorGreen"] = (self.colors[:, 1] * max_value).astype(np.uint8)
+            raw_data["colorBlue"] = (self.colors[:, 2] * max_value).astype(np.uint8)
 
         e57.write_scan_raw(raw_data)
 
@@ -75,12 +82,14 @@ class PointCloud:
             b = np.array(las.blue)
             self.colors = np.stack((r, g, b), axis=1)
         else:
-            self.colors = np.zeros_like(self.points, dtype=np.uint8)
+            self.colors = np.zeros_like(self.points, dtype=self.color_default_dtype)
 
         if intensities_name is not None:
             self.intensities = np.array(las[intensities_name])
         else:
-            self.intensities = np.zeros(shape=(len(self.points), ), dtype=np.float32)
+            self.intensities = np.zeros(shape=(len(self.points),), dtype=self.intensities_default_dtype)
+
+        x = 0
 
     def write_las(self, filename: str):
         point_format = 3
@@ -145,7 +154,7 @@ class PointCloud:
             self.colors = np.zeros_like(self.points, dtype=np.uint8)
 
         if self.intensities is None:
-            self.intensities = np.zeros(shape=(len(self.points), ), dtype=np.float32)
+            self.intensities = np.zeros(shape=(len(self.points),), dtype=np.float32)
 
     # https://python-plyfile.readthedocs.io/en/latest/usage.html#creating-a-ply-file
     def write_ply(self, filename: str):
@@ -179,3 +188,27 @@ class PointCloud:
         data['scalar_Intensity'] = self.intensities
         el.data = data
         PlyData([el]).write(filename)
+
+    def map_field_names(self, from_field_names: list[str]) -> dict:
+        mapping = {'x': None, 'y': None, 'z': None, 'r': None, 'g': None, 'b': None, 'intensity': None}
+
+        for field_name in from_field_names:
+            field_name_lowered: str = field_name.lower()
+            if mapping['x'] is None and "x" in field_name_lowered:
+                mapping['x'] = field_name
+            elif mapping['y'] is None and "y" in field_name_lowered:
+                mapping['y'] = field_name
+            elif mapping['z'] is None and "z" in field_name_lowered:
+                mapping['z'] = field_name
+            elif mapping['r'] is None and (field_name_lowered == "r" or "red" in field_name_lowered):
+                mapping['r'] = field_name
+            elif mapping['g'] is None and (field_name_lowered == "g" or "green" in field_name_lowered):
+                mapping['g'] = field_name
+            elif mapping['b'] is None and (field_name_lowered == "b" or "blue" in field_name_lowered):
+                mapping['b'] = field_name
+
+            # Get both 'intensity' and 'intensities'
+            elif mapping['intensity'] is None and "intensit" in field_name_lowered:
+                mapping['intensity'] = field_name
+
+        return mapping
